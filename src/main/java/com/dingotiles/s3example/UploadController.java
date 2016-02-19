9b63937c65aae5c23f1a4b15a97d3feb79b79306
Justin Carter <jcarter@starkandwayne.com>
2016-02-19 16:39:50 +0100

    Upload form in home.html view

--- src/main/java/com/dingotiles/s3example/UploadController.java
+++ src/main/java/com/dingotiles/s3example/UploadController.java
@@ -1,10 +1,9 @@
 package com.dingotiles.s3example;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Created by jcarter on 19/02/16.
@@ -16,5 +15,13 @@ public class UploadController {
     @RequestMapping(method = RequestMethod.GET)
     String home() {
         return "home";
+    }
+
+    @RequestMapping(value = "/upload")
+    public String importParse(
+            //@RequestParam("file") MultipartFile myFile,
+            final RedirectAttributes redirectAttributes) {
+            redirectAttributes.addFlashAttribute("flash.message", "File was uploaded!");
+         return "redirect:/";
     }
 }
